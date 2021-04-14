58d46379a63f65c0192a70dab3a2883f6326d92f
Carsten Behring <carsten.behring@gmail.com>
2021-04-14 22:43:40 +0200

    bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject scicloj/metamorph "0.1.0"
+(defproject scicloj/metamorph "0.2.0-SNAPSHOT"
   :description "Context based pipeline."
   :url "https://github.com/scicloj/metamorph"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
